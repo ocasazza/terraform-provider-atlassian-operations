82856f8f99a576864827a17986242e9c705a5310
İbrahim Aral Özkaya <iozkaya@atlassian.com>
2024-09-25 12:34:48 +0300

    Implemented User Data Source

---
+++ internal/provider/dataModels/user_data_source_model.go
@@ -0,0 +1,113 @@
+package dataModels
+
+import (
+	"github.com/atlassian/jsm-ops-terraform-provider/internal/dto"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type (
+	UserDataSourceModel struct {
+		Id               types.String                     `tfsdk:"id"`
+		AccountType      types.String                     `tfsdk:"account_type"`
+		Active           types.Bool                       `tfsdk:"active"`
+		ApplicationRoles []ApplicationRoleDataSourceModel `tfsdk:"application_roles"`
+		AvatarUrls       *AvatarUrlsBeanDataSourceModel   `tfsdk:"avatar_urls"`
+		FullName         types.String                     `tfsdk:"full_name"`
+		EmailAddress     types.String                     `tfsdk:"email_address"`
+		Expand           types.String                     `tfsdk:"expand"`
+		Groups           []GroupNameDataSourceModel       `tfsdk:"groups"`
+		Locale           types.String                     `tfsdk:"locale"`
+		TimeZone         types.String                     `tfsdk:"timezone"`
+	}
+	ApplicationRoleDataSourceModel struct {
+		DefaultGroups        []types.String             `tfsdk:"default_groups"`
+		DefaultGroupsDetails []GroupNameDataSourceModel `tfsdk:"default_groups_details"`
+		Defined              types.Bool                 `tfsdk:"defined"`
+		GroupDetails         []GroupNameDataSourceModel `tfsdk:"group_details"`
+		Groups               []types.String             `tfsdk:"groups"`
+		HasUnlimitedSeats    types.Bool                 `tfsdk:"has_unlimited_seats"`
+		Key                  types.String               `tfsdk:"key"`
+		Name                 types.String               `tfsdk:"name"`
+		NumberOfSeats        types.Int32                `tfsdk:"number_of_seats"`
+		Platform             types.Bool                 `tfsdk:"platform"`
+	}
+	GroupNameDataSourceModel struct {
+		GroupId types.String `tfsdk:"group_id"`
+		Name    types.String `tfsdk:"name"`
+		Self    types.String `tfsdk:"self"`
+	}
+	AvatarUrlsBeanDataSourceModel struct {
+		A16x16 types.String `tfsdk:"a_16x16"`
+		A24x24 types.String `tfsdk:"a_24x24"`
+		A32x32 types.String `tfsdk:"a_32x32"`
+		A48x48 types.String `tfsdk:"a_48x48"`
+	}
+)
+
+func UserDtoToModel(dto dto.UserDto) UserDataSourceModel {
+	model := UserDataSourceModel{
+		Id:               types.StringValue(dto.AccountId),
+		AccountType:      types.StringValue(string(dto.AccountType)),
+		Active:           types.BoolValue(dto.Active),
+		ApplicationRoles: make([]ApplicationRoleDataSourceModel, dto.ApplicationRoles.Size),
+		AvatarUrls:       AvatarUrlsBeanDtoToModel(dto.AvatarUrls),
+		FullName:         types.StringValue(dto.DisplayName),
+		EmailAddress:     types.StringValue(dto.EmailAddress),
+		Expand:           types.StringValue(dto.Expand),
+		Groups:           make([]GroupNameDataSourceModel, dto.Groups.Size),
+		Locale:           types.StringValue(dto.Locale),
+		TimeZone:         types.StringValue(dto.TimeZone),
+	}
+	for i, applicationRole := range dto.ApplicationRoles.Items {
+		model.ApplicationRoles[i] = ApplicationRoleDtoToModel(applicationRole)
+	}
+	for i, group := range dto.Groups.Items {
+		model.Groups[i] = GroupNameDtoToModel(group)
+	}
+	return model
+}
+
+func AvatarUrlsBeanDtoToModel(dto dto.AvatarUrlsBeanDto) *AvatarUrlsBeanDataSourceModel {
+	return &AvatarUrlsBeanDataSourceModel{
+		A16x16: types.StringValue(dto.A16x16),
+		A24x24: types.StringValue(dto.A24x24),
+		A32x32: types.StringValue(dto.A32x32),
+		A48x48: types.StringValue(dto.A48x48),
+	}
+}
+
+func GroupNameDtoToModel(dto dto.GroupNameDto) GroupNameDataSourceModel {
+	return GroupNameDataSourceModel{
+		GroupId: types.StringValue(dto.GroupId),
+		Name:    types.StringValue(dto.Name),
+		Self:    types.StringValue(dto.Self),
+	}
+}
+
+func ApplicationRoleDtoToModel(dto dto.ApplicationRoleDto) ApplicationRoleDataSourceModel {
+	model := ApplicationRoleDataSourceModel{
+		DefaultGroups:        make([]types.String, len(dto.DefaultGroups)),
+		DefaultGroupsDetails: make([]GroupNameDataSourceModel, len(dto.DefaultGroupsDetails)),
+		Defined:              types.BoolValue(dto.Defined),
+		GroupDetails:         make([]GroupNameDataSourceModel, len(dto.GroupDetails)),
+		Groups:               make([]types.String, len(dto.Groups)),
+		HasUnlimitedSeats:    types.BoolValue(dto.HasUnlimitedSeats),
+		Key:                  types.StringValue(dto.Key),
+		Name:                 types.StringValue(dto.Name),
+		NumberOfSeats:        types.Int32Value(dto.NumberOfSeats),
+		Platform:             types.BoolValue(dto.Platform),
+	}
+	for i, defaultGroup := range dto.DefaultGroups {
+		model.DefaultGroups[i] = types.StringValue(defaultGroup)
+	}
+	for i, defaultGroupDetail := range dto.DefaultGroupsDetails {
+		model.DefaultGroupsDetails[i] = GroupNameDtoToModel(defaultGroupDetail)
+	}
+	for i, group := range dto.GroupDetails {
+		model.GroupDetails[i] = GroupNameDtoToModel(group)
+	}
+	for i, group := range dto.Groups {
+		model.Groups[i] = types.StringValue(group)
+	}
+	return model
+}
---
+++ internal/provider/schemaAttributes/user_data_source_attributes.go
@@ -0,0 +1,145 @@
+package schemaAttributes
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var UserDataSourceAttributes = map[string]schema.Attribute{
+	"id": schema.StringAttribute{
+		Description: "The account ID of the user",
+		Computed:    true,
+	},
+	"account_type": schema.StringAttribute{
+		Description: "The account type of the user",
+		Computed:    true,
+	},
+	"active": schema.BoolAttribute{
+		Description: "The active status of the user",
+		Computed:    true,
+	},
+	"application_roles": schema.ListNestedAttribute{
+		Description: "The application roles of the user",
+		Computed:    true,
+		NestedObject: schema.NestedAttributeObject{
+			Attributes: ApplicationRoleDataSourceAttributes,
+		},
+	},
+	"avatar_urls": schema.SingleNestedAttribute{
+		Description: "The avatar URLs of the user",
+		Computed:    true,
+		Attributes:  AvatarUrlsBeanDataSourceAttributes,
+	},
+	"full_name": schema.StringAttribute{
+		Description: "The display name of the user",
+		Computed:    true,
+	},
+	"email_address": schema.StringAttribute{
+		Description: "The email address of the user",
+		Required:    true,
+	},
+	"expand": schema.StringAttribute{
+		Description: "The expand of the user",
+		Computed:    true,
+	},
+	"groups": schema.ListNestedAttribute{
+		Description: "The groups of the user",
+		Computed:    true,
+		NestedObject: schema.NestedAttributeObject{
+			Attributes: GroupNameDataSourceAttributes,
+		},
+	},
+	"locale": schema.StringAttribute{
+		Description: "The locale of the user",
+		Computed:    true,
+	},
+	"timezone": schema.StringAttribute{
+		Description: "The time zone of the user",
+		Computed:    true,
+	},
+}
+
+var ApplicationRoleDataSourceAttributes = map[string]schema.Attribute{
+	"default_groups": schema.ListAttribute{
+		ElementType: types.StringType,
+		Computed:    true,
+		Description: "The default groups of the application role",
+	},
+	"default_groups_details": schema.ListNestedAttribute{
+		Description: "The default groups details of the application role",
+		Computed:    true,
+		NestedObject: schema.NestedAttributeObject{
+			Attributes: GroupNameDataSourceAttributes,
+		},
+	},
+	"defined": schema.BoolAttribute{
+		Description: "The defined status of the application role",
+		Computed:    true,
+	},
+	"group_details": schema.ListNestedAttribute{
+		Description: "The group details of the application role",
+		Computed:    true,
+		NestedObject: schema.NestedAttributeObject{
+			Attributes: GroupNameDataSourceAttributes,
+		},
+	},
+	"groups": schema.ListAttribute{
+		ElementType: types.StringType,
+		Computed:    true,
+		Description: "The groups of the application role",
+	},
+	"has_unlimited_seats": schema.BoolAttribute{
+		Description: "The has unlimited seats status of the application role",
+		Computed:    true,
+	},
+	"key": schema.StringAttribute{
+		Description: "The key of the application role",
+		Computed:    true,
+	},
+	"name": schema.StringAttribute{
+		Description: "The name of the application role",
+		Computed:    true,
+	},
+	"number_of_seats": schema.Int32Attribute{
+		Description: "The number of seats of the application role",
+		Computed:    true,
+	},
+	"platform": schema.BoolAttribute{
+		Description: "The platform status of the application role",
+		Computed:    true,
+	},
+}
+
+var GroupNameDataSourceAttributes = map[string]schema.Attribute{
+	"group_id": schema.StringAttribute{
+		Description: "The group ID",
+		Computed:    true,
+	},
+	"name": schema.StringAttribute{
+		Description: "The name of the group",
+		Computed:    true,
+	},
+	"self": schema.StringAttribute{
+		Description: "The self of the group",
+		Computed:    true,
+	},
+}
+
+var AvatarUrlsBeanDataSourceAttributes = map[string]schema.Attribute{
+	"a_16x16": schema.StringAttribute{
+		Description: "The 16x16 avatar URL",
+		Computed:    true,
+	},
+	"a_24x24": schema.StringAttribute{
+		Description: "The 24x24 avatar URL",
+		Computed:    true,
+	},
+	"a_32x32": schema.StringAttribute{
+		Description: "The 32x32 avatar URL",
+		Computed:    true,
+	},
+	"a_48x48": schema.StringAttribute{
+		Description: "The 48x48 avatar URL",
+		Computed:    true,
+	},
+}
